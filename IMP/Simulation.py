84e3d6e49f90e32dc2c0838fec568c806ce697c4
Charlotte Out <charlotte_out@hotmail.com>
2022-09-04 17:53:28 +0200

    The plots code added!
    Co-Authored-By: Sajjad Hesamipour <34142871+sajjadhesami@users.noreply.github.com>

--- IMP/Simulation.py
+++ IMP/Simulation.py
@@ -14,20 +14,20 @@ def KClique(j,c):
     #connect them with edges to make a cycle
     for i in range(c):
         G.add_edge(i, i+j)
-    print(G)
-    print(G.number_of_nodes())
+    #print(G)
+    #print(G.number_of_nodes())
     return G
 
 def KCliqueExpander(j,c,d):
     #something does not work here
     KC = KClique(j,c)
-    print("KCLIQUE", KC, KC.nodes())
-    print(j*c)
+    #print("KCLIQUE", KC, KC.nodes())
+    #print(j*c)
     mapping = dict(zip(KC, range(0, KC.number_of_nodes() - 1)))
     #temp_graph = nx.relabel_nodes(temp_graph, mapping)
     RRG = nx.random_regular_graph(d, j*c)
     RRG = nx.relabel_nodes(RRG, mapping)
-    print("RRG", RRG, RRG.nodes())
+    #print("RRG", RRG, RRG.nodes())
     G = nx.compose(RRG, KC)
     return G
 
@@ -36,23 +36,24 @@ def GetSNlikeGraph(graph, type_graph):
     temp_graph = nx.read_edgelist(os.path.join(abs_path, graph), create_using=nx.Graph(), nodetype=int)
     mapping = dict(zip(temp_graph, range(0, temp_graph.number_of_nodes() - 1)))
     temp_graph = nx.relabel_nodes(temp_graph, mapping)
-    print("num nodes", temp_graph.number_of_nodes())
-    print('num edges', temp_graph.number_of_edges())
+    #print("num nodes", temp_graph.number_of_nodes())
+    #print('num edges', temp_graph.number_of_edges())
     total = sum(j for i, j in list(temp_graph.degree(temp_graph.nodes)))
     av_deg = total / temp_graph.number_of_nodes()
-    print("av_deg", av_deg)
+    #print("av_deg", av_deg)
     p = total / (temp_graph.number_of_nodes() * (temp_graph.number_of_nodes() - 1))
     if type_graph == 'ER':
-        print("ER graph")
+        #print("ER graph")
         our_graph = nx.fast_gnp_random_graph(n=temp_graph.number_of_nodes(), p=p)
     if type_graph == 'BA':
-        print("BA graph")
+        #print("BA graph")
         our_graph = nx.barabasi_albert_graph(n=temp_graph.number_of_nodes(), m=int(av_deg))
     if type_graph == 'SN':
         our_graph = temp_graph
     return our_graph
 
 def GetGraph(SNtype, graph, type_graph, j,c,d):
+    print("in get graph")
     if SNtype:
         our_graph = GetSNlikeGraph(graph, type_graph)
     else:
@@ -67,7 +68,7 @@ def GetGraph(SNtype, graph, type_graph, j,c,d):
     return our_graph
 
 def GetInitialOpinions(graph, p, gray_p):
-
+    print("in get initial opinions")
     for node in graph.nodes:
         if (random.random() < gray_p):
             #these are the nodes that will have vote gray and thus are not affected by the neighbours
@@ -98,9 +99,6 @@ def Simulation(graph, SNtype, type_graph,p, gray_p,k, tresh, d, j, c):
     #now we consider the update rule
     stop = 0
     phase = 0
-    cur_num_black = 0
-    cur_num_white = 0
-    cur_num_gray = 0
     round = 1
 
     #initialize the field temp_vote and stamp
@@ -110,31 +108,41 @@ def Simulation(graph, SNtype, type_graph,p, gray_p,k, tresh, d, j, c):
         our_graph.nodes[node]['stamp'] = 0
 
     blacknodes_initial = [node for node in our_graph.nodes if our_graph.nodes[node]['vote'] == 1]
-    print("blacknodes initial", len(blacknodes_initial))
+    cur_num_black = len(blacknodes_initial)
+    list_num_black = [cur_num_black]
+
     gray_nodes_initial = [node for node in our_graph.nodes if our_graph.nodes[node]['vote'] == 0]
-    num_gray_nodes_initial = len(gray_nodes_initial)
-    print("gray_nodes_initial", num_gray_nodes_initial)
+    cur_num_gray = len(gray_nodes_initial)
+    list_num_gray = [cur_num_gray]
+
+
+
+    whitenodes_initial = [node for node in our_graph.nodes if our_graph.nodes[node]['vote'] == -1]
+    cur_num_white = len(whitenodes_initial)
+    list_num_white = [cur_num_white]
+
 
     #now (in contrast to the prev project) we proceed in phases, where each phase consists of k rounds.
     while stop != 1:
         phase = phase + 1
-        print("phase", phase)
+        #print("phase", phase)
         change = 0
-        print("change zero", change)
-        for round in range(k):
-            print("round", round)
+        for round in range(k+1):
+            #print("round", round)
             for node in [node for node in our_graph.nodes if our_graph.nodes[node]['vote'] == 1]:
-                print("black node", node)
-                our_graph.nodes[node]['stamp'] += 1
+                our_graph.nodes[node]['stamp'] = our_graph.nodes[node]['stamp'] + 1
                 #the node has been black for k rounds and becomes gray
                 if our_graph.nodes[node]['stamp'] == k:
+                    #print("becoming gray", node)
                     our_graph.nodes[node]['vote'] = 0
+                    cur_num_gray = cur_num_gray + 1
+                    cur_num_black = cur_num_black - 1
                 else:
+                    #print("node", node)
                     neighlist = list(our_graph.adj[node])
-                    print("neighlist", neighlist)
+                    #print("neighlist",neighlist)
                     #only consider the white neighbors these are the only ones that can be influenced
                     for neigh in [neigh for neigh in neighlist if our_graph.nodes[neigh]['vote'] == -1]:
-                        print("neigh", neigh)
                         #manually add the nodes to their own neighborhoods
                         neighset = set(our_graph.adj[node])
                         neighset.add(node)
@@ -143,38 +151,41 @@ def Simulation(graph, SNtype, type_graph,p, gray_p,k, tresh, d, j, c):
                         intersection_neigh = neighset.intersection(neighsetneigh)
                         union_neigh = neighset.union(neighsetneigh)
                         jaccard_sim = Decimal(len(intersection_neigh) / len(union_neigh))
-                        print("jaccard_sim", jaccard_sim)
+                        #print("jaccard_sim", jaccard_sim)
                         denom = Decimal(2**(our_graph.nodes[node]['stamp']))
                         r = (jaccard_sim / denom)
                         rand = random.random()
-                        print("r and rand", r, rand)
+                        #print("r and rand", r, rand)
                         if rand < r:
+                            #print("neigh",neigh) #see if there are duplicates
                             our_graph.nodes[neigh]['vote'] = 1
                             change = change + 1
-                            print("change", change)
+                            cur_num_black = cur_num_black + 1
+                            cur_num_white = cur_num_white - 1
+                            #print("change", change)
+
+            print("round", round, "phase", phase, cur_num_gray, cur_num_white, cur_num_black)
+            list_num_white.append(cur_num_white)
+            list_num_black.append(cur_num_black)
+            list_num_gray.append(cur_num_gray)
+            print("listnumwhite", list_num_white)
+            print("listnumblack", list_num_black)
+            print("listnumgray", list_num_gray)
+        print("change", change)
+        if change == 0:
+            stop = 1
 
 
 
-            if change == 0:
-                stop = 1
 
-    #now count how many white nodes we have. If the num of white nodes - p*n (num of initial gray nodes in exp)
-    # is larger than threshold (a parameter) we say that the rumour has spread
-    whitenodes = [node for node in our_graph.nodes if our_graph.nodes[node]['vote'] == -1]
-    blacknodes = [node for node in our_graph.nodes if our_graph.nodes[node]['vote'] == 1]
-    graynodes = [node for node in our_graph.nodes if our_graph.nodes[node]['vote'] == 0]
-    num_whitenodes = len(whitenodes)
-    num_blacknodes = len(blacknodes)
-    num_graynodes = len(graynodes)
-    print("num_whitenodes", num_whitenodes)
-    n = our_graph.number_of_nodes()
-    print("n", n)
-    ratio = (num_blacknodes + num_graynodes - num_gray_nodes_initial)/n
-    print("ratio", ratio)
-    if  ratio > tresh:
-        winner = "black"
-    else:
-        winner = "white"
-    print(winner)
-    return winner
+
+
+
+
+
+
+
+
+
+    return [list_num_white, list_num_black, list_num_gray]
 
--- IMP/main.py
+++ IMP/main.py
@@ -4,8 +4,8 @@ ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.append(ROOT_DIR)
 
 from IMP import twitter_loc
-from IMP.simulation import Simulation
+from IMP.Simulation import Simulation
 
 if __name__ == "__main__":
-    Simulation(graph="whatev", SNtype=False, type_graph="KCliqueExpander", p=0.1, gray_p=0.1, k=4, c=20, tresh=0.10,
-               d=1, j=4)
+    Simulation(graph="whatev", SNtype=False, type_graph="KClique", p=1/50000, gray_p=0, k=5, c=10000, tresh=0.10,
+               d=0, j=5)
