4d3a3f4b193e573908ed3fd05ce313eb73cce15a
Charlotte Out <charlotte_out@hotmail.com>
2023-01-16 12:20:01 +0100

    added requirements file

--- IMP/main.py
+++ IMP/main.py
@@ -6,16 +6,16 @@ sys.path.append(ROOT_DIR)
 
 
 import matplotlib.pyplot as plt
-from IMP import fb_loc, twitter_loc
+from IMP import fb_loc, twitter_loc, gplus_loc
 from IMP.Simulation import *
 from IMP.averaging import *
 if __name__ == "__main__":
     #dict_counter_measure = {'id':COUNTER_MEASURE_COMMUNITY_DETECTION, 'p_orange_h': 0.05, 'p_orange_r':0.1}
-    dict_counter_measure = {'id':COUNTER_MEASURE_NONE, 'num_green':1, 'start_time':4, 'green_spreading_ratio':0.5,
+    dict_counter_measure = {'id':COUNTER_MEASURE_COMMUNITY_DETECTION, 'num_green':1, 'start_time':4, 'green_spreading_ratio':0.5,
                             'p_orange_h':0.05, 'p_orange_r':0.1, 'p_coinflip':0.7, 'frac_green':0.1, 'threshold_detection':0.05,
                             'threshold_block':0.05}
     dict_args = {"degree_of_supernodes":150, "number_of_supernodes": 1000, "nodes_in_clique": 16}
 
 
 
-    list_gray = BigSimulation(num_runs=1, graph_loc=musea_DE_loc, type_graph='SN', num_red=1, k=5, dict_args=dict_args,dict_counter_measure=dict_counter_measure)
+    list_gray = BigSimulation(num_runs=1, graph_loc= gplus_loc,type_graph='SN', num_red=1, k=5, dict_args=dict_args,dict_counter_measure=dict_counter_measure)
